6bbb57a1d5a290021358e9d88503e9a649de57b9
srimathiprakash <srimathiprakash04@gmail.com>
2025-07-26 15:29:33 +0530

    Update pattern Generator.c

--- pattern Generator.c
+++ pattern Generator.c
@@ -3,12 +3,12 @@
 int main(){
 int i,j,rows;
 printf("Enter number of rows:");
-scanf("%d",@rows);
+scanf("%d",&rows);
 
 
 for(i=1;i<=rows;i++){
     for(j=1;j<=i;j++){
-        printf("%d,"j);
+        printf("%d",j);
     }
     printf("\n");
 }
